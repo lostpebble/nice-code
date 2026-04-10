549b044cb7cec2f366ce6fac69a439762792917a
Paul <lostpebble@protonmail.com>
2026-04-10 03:41:30 +0200

    feat: added in documentation web app

---
+++ packages/documentation/astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "@nice-error",
+      social: [{ icon: "github", label: "GitHub", href: "https://github.com/lostpebble/nice-error" }],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
