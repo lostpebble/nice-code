7f11796fd4d0d8521b32d59aca63757f7158975c
Paul <lostpebble@protonmail.com>
2026-04-13 02:35:02 +0200

    chore: minor demo frontend start things

--- packages/demo-frontend/src/App.tsx
+++ packages/demo-frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <h1>Demo Frontend</h1>
+    </div>
+  );
 }
 
 export default App;
