65f6c2830391c3b001b7062e97b1fee14fb9da46
Paul <lostpebble@protonmail.com>
2026-04-15 03:52:59 +0200

    feat: use prototools for github actions

--- packages/documentation/astro.config.mjs
+++ packages/documentation/astro.config.mjs
@@ -5,8 +5,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE ?? "https://lostpebble.github.io",
-  base: process.env.BASE_PATH ?? "/nice-error",
+  site: process.env.SITE ?? "https://lostpebble.github.io/nice-error",
+  base: process.env.BASE_PATH ?? "/",
   integrations: [
     starlight({
       title: "nice-error",
