a08b7dea02b0c25641755fc0f25102c010e44081
Paul <lostpebble@protonmail.com>
2026-04-17 21:30:41 +0200

    chore: update windows dev script

--- windows_dev_env/run_all.ps1
+++ windows_dev_env/run_all.ps1
@@ -1,5 +1,5 @@
-wt -w 0 nt -d ".\packages\core" pwsh -NoExit -Command "bun run vitest" `; `
-      nt -d ".\packages\core" pwsh -NoExit -Command "bun run type-check-watch" `; `
+wt -w 0 nt -d ".\packages\nice-error" pwsh -NoExit -Command "bun run vitest" `; `
+      nt -d ".\packages\nice-error" pwsh -NoExit -Command "bun run type-check-watch" `; `
       nt -d ".\packages\common-errors" pwsh -NoExit -Command "bun run type-check-watch" `; `
       nt -d ".\packages\nice-action" pwsh -NoExit -Command "bun run vitest" `; `
       nt -d ".\packages\nice-action" pwsh -NoExit -Command "bun run type-check-watch" `; `
